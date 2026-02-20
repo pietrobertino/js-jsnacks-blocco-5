23ff7636802d637e6358901150bccef31c2ecc86
pietrobertino <pietrobertino20@gmail.com>
2026-02-20 15:30:36 +0100

    complete fourth snack

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(post => post.author);
+
+console.log(authors);
