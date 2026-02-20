6125f45c8945217b0c67756dab3b908d68dee0e1
pietrobertino <pietrobertino20@gmail.com>
2026-02-20 15:32:16 +0100

    complete fifth snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNumbers = nums.filter(number => number%2===0);
+
+console.log(evenNumbers);
