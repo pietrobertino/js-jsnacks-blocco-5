b49d14e71b1cc067f7c2214e5366eb370aed5f4f
pietrobertino <pietrobertino20@gmail.com>
2026-02-20 15:40:43 +0100

    complete eighth snack

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentMarcoLanci = students.find(student => student.name === "Marco Lanci");
+
+const classOfMarcoLanci = studentMarcoLanci.class;
+
+console.log(classOfMarcoLanci);
